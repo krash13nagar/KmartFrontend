710a99d9c4acae2823d154c0310ffd1b733950b2
Krash Nagar <dpsvnkras12102@gmail.com>
2023-07-26 16:19:30 +0530

    bug resolved in api callling

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://kmart-backend.vercel.app/',
+      target: 'https://kmart-backend.vercel.app',
       changeOrigin: true,
     })
   );  
